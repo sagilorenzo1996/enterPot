a2a1172da337cb0955cb2394769e213fe8c9d1c2
sagilorenzo1996 <sagilorenzo1996@gmail.com>
2019-03-24 19:20:47 +0530

    DB

--- src/main/java/com/finalYearProject/enterPot/controller/OrderController.java
+++ src/main/java/com/finalYearProject/enterPot/controller/OrderController.java
@@ -10,7 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.math.BigDecimal;
+import java.util.Date;
 import java.util.Optional;
 
 @RestController
@@ -26,6 +28,10 @@ public class OrderController {
     @Autowired
     OrderRepository orderRepository;
 
+    private String ORDERPLACED = "placed";
+
+    @CrossOrigin
+    @Transactional
     @PostMapping("/")
     public Long createOrder(@RequestParam Long customer, @RequestParam Long item, @RequestParam int quantity, @RequestBody BigDecimal total)throws ResourceNotFoundException {
         Optional<Customer> customer1= customerRepository.findById(customer);
@@ -33,8 +39,17 @@ public class OrderController {
         if(!customer1.isPresent() || !item1.isPresent()){
             throw new ResourceNotFoundException("Can't Process Order");
         }
+        Order order = new Order();
         if(quantity>0 && total.signum()>0){
-
+            order.setCustomerId(customer1.get().getId());
+            order.setItemId(item1.get().getId());
+            order.setQuantity(quantity);
+            order.setTotal(total);
+            order.setLocation(customer1.get().getAddressLineOne()+","+customer1.get().getAddressLineTwo()+","+customer1.get().getCity()+","+customer1.get().getCountry()+","+customer1.get().getPostalCode());
+            order.setOrderedDate(new Date());
+            order.setShippingMode("ship");
+            order.setStatus(this.ORDERPLACED);
         }
+        return orderRepository.save(order).getId();
     }
 }
