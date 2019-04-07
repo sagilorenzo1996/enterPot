769e12429a64e9f74fa5cc79ab3b909f44f1091c
sagilorenzo1996 <sagilorenzo1996@gmail.com>
2019-04-07 17:44:26 +0530

    DB

--- src/main/resources/static/js/init.js
+++ src/main/resources/static/js/init.js
@@ -6,6 +6,8 @@
     $('.dropdown-trigger').dropdown();
     $('.fixed-action-btn').floatingActionButton();
     $('.tooltipped').tooltip();
+    $('.datepicker').datepicker();
+    $('select').formSelect();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
